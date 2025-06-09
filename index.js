e35533e4a3a5fe9cc74ce9f9abd39ef1ddd9188c
sheiza-jagemi <sheiza.jagemi@student.moringaschool.com>
2025-06-09 11:51:35 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+describe('index.js', function() {
+  it('does not allow rides over 2500 feet', function() {
+    expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');
+  });
+});
+//     it('does not allow rides over 2500 feet', function() {
+//       expect(calculatesFarePrice(34, 24)).to.equal('cannot travel that far');        
+//     });
+//   });
+// });      
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}   
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
